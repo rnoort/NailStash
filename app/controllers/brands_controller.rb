00aaba208e4ba58f6c7442f71c7f6367c6a6ce3d
Robijn Noort <rrnoort@gmail.com>
2023-03-21 21:27:23 +0100

    feat: added more methods and changed the model for brands

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,10 +1,50 @@
 class BrandsController < ApplicationController
   def index
+    @brands = Brand.all
   end
 
   def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.create(brand_params)
+
+    if @brand.save
+      redirect_to brands_path
+    else
+      render :new, status: :unprocessable_identity
+    end
   end
 
   def edit
+    @brand = Brand.find(params[:id])
   end
+
+  def show
+    @brand = Brand.find(params[:id])
+  end
+
+  def update
+    @brand = Brand.find(params[:id])
+
+    if @brand.update(brand_params)
+      redirect_to brands_path
+    else
+      render :edit, status: :unprocessable_identity
+    end
+  end
+
+  def destroy
+    @brand = Brand.find(params[:id])
+
+    @brand.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def brand_params
+      params.require(:brand).permit(:name)
+    end
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,5 @@
 class Brand < ApplicationRecord
-    has_many :nails, dependent: :destroy
+    has_many :nails, dependent: :destroy, counter_cache: true
+
+    validates :name, presence: true
 end
