0bdb6e0ebecd21eced470bdedfa3762e6f673f0a
Robijn Noort <rrnoort@gmail.com>
2023-03-21 16:11:49 +0100

    refactor: created new migration

--- db/migrate/20230321150910_create_nails.rb
+++ db/migrate/20230321150910_create_nails.rb
@@ -1,6 +1,7 @@
 class CreateNails < ActiveRecord::Migration[7.0]
   def change
     create_table :nails do |t|
+      t.references :brand, null: false, foreign_key: true
       t.integer :code
       t.string :color
       t.integer :amount
